823b5a2d69a4a9a0ac01c2676bd0340168f30d17
Alan Gonzalez <argaalan@hotmail.com>
2018-11-08 15:45:35 -0600

    No major changes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
+        console.log("HELLO")
         if (scroll > 300) {
           $(".navbar").css("background" , "blue");
         }
-  
         else{
             $(".navbar").css("background" , "#333");  	
         }
